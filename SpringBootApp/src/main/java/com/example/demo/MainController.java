a42c4da24e30c72065887e753b80d2de23748862
shin-nkt-09 <shin.nkt.09@gmail.com>
2022-04-30 17:33:11 +0900

    新規登録画面レイアウト作成

--- SpringBootApp/src/main/java/com/example/demo/MainController.java
+++ SpringBootApp/src/main/java/com/example/demo/MainController.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 @Controller
-public class TopController {
+public class MainController {
     @GetMapping("/top")
-    public String index(Model model) {
+    public String top(Model model) {
 //        model.addAttribute("message", "Hello Thymeleaf !!");
         return "top";
+    }
+
+    @GetMapping("/insert")
+    public String insert(Model model) {
+//        model.addAttribute("message", "Hello Thymeleaf !!");
+        return "insert";
     }
 }
